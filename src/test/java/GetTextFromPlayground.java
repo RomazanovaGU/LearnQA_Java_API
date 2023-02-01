5e99b4084cc263093901e5c44cfe2675bb0c33d1
Galina Romazanova <galina.romazanova@revelsystems.com>
2023-02-01 10:12:14 +0300

    GetTextFromPlayground

---
+++ src/test/java/GetTextFromPlayground.java
@@ -0,0 +1,12 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+public class GetTextFromPlayground {
+    @Test
+    public void testGetTextFromPlayground(){
+        Response response = RestAssured.
+                get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
