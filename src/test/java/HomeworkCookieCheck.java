47aed8df53f0ff56b1536bdf9a611bfe7d6a5d38
Galina Romazanova <galina.romazanova@revelsystems.com>
2023-02-18 18:45:50 +0300

    Check Homework_cookie

---
+++ src/test/java/HomeworkCookieCheck.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HomeworkCookieCheck {
+    @Test
+    public void cookieCheck() {
+        Response response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        Map<String, String> cookies = response.getCookies();
+        assertTrue(cookies.containsKey("HomeWork"), "Responce has unexpected cookie" + cookies);
+        if (cookies.containsKey("HomeWork")){
+            assertEquals ("hw_value", response.getCookie("HomeWork"), "Responce has unexpected value of cookie" + cookies.values());
+        }
+    };
+
+}
+
