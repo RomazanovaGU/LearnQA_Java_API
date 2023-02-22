b1e6d618e35143a9afc44b22132b91eb71b28d90
Galina Romazanova <galina.romazanova@revelsystems.com>
2023-02-22 21:36:26 +0300

    UserRegisterTest with invalid email format, short and long name parameters, missed parameter

--- src/test/java/lib/ApiCoreRequests.java
+++ src/test/java/lib/ApiCoreRequests.java
@@ -5,6 +5,7 @@ import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
@@ -42,5 +43,57 @@ public class ApiCoreRequests {
                 .body(authData)
                 .post(url)
                 .andReturn();
+    }
+    @Step("Make a POST-request for user registration with invalid email parameter")
+    public Response makePostRequestWithInvalidEmailParam (String url, String invalidEmail){
+        Map <String, String> updatedUserData = DataGenerator.getRegistrationData();
+            updatedUserData.replace("email", updatedUserData.get("email"), invalidEmail);
+            return given()
+                    .filter(new AllureRestAssured())
+                    .body(updatedUserData)
+                    .post(url)
+                    .andReturn();
+    }
+
+    @Step("Make a POST-request for user registration with short value of name parameter")
+    public Response makePostRequestWithShortNameParam (String url, String nameParameter){
+        Map <String, String> updatedUserData = DataGenerator.getRegistrationData();
+        String shortValue = "l";
+        if (updatedUserData.containsKey(nameParameter)){
+            updatedUserData.replace(nameParameter, updatedUserData.get(nameParameter), shortValue);
+            return given()
+                    .filter(new AllureRestAssured())
+                    .body(updatedUserData)
+                    .post(url)
+                    .andReturn();
+        }
+        return null;
+    }
+    @Step("Make a POST-request for user registration with long value of name parameter")
+    public Response makePostRequestWithLongNameParam (String url, String nameParameter){
+        Map <String, String> updatedUserData = DataGenerator.getRegistrationData();
+        String shortValue = "learnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqalearnqa";
+        if (updatedUserData.containsKey(nameParameter)){
+            updatedUserData.replace(nameParameter, updatedUserData.get(nameParameter), shortValue);
+            return given()
+                    .filter(new AllureRestAssured())
+                    .body(updatedUserData)
+                    .post(url)
+                    .andReturn();
+        }
+        return null;
+    }
+    @Step("Make a POST-request for user registration with one missed parameter")
+    public Response makePostRequestWithMissedParam (String url, String missedParam){
+        Map <String, String> updatedUserData = DataGenerator.getRegistrationData();
+        if (updatedUserData.containsKey(missedParam)){
+            updatedUserData.remove(missedParam);
+            return given()
+                    .filter(new AllureRestAssured())
+                    .body(updatedUserData)
+                    .post(url)
+                    .andReturn();
+        }
+        return null;
     }
 }
--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -6,12 +6,15 @@ import io.qameta.allure.Feature;
 import io.restassured.RestAssured;
 import io.restassured.http.Headers;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import lib.Assertions;
 import lib.BaseTestCase;
 import lib.DataGenerator;
 import lib.ApiCoreRequests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -26,19 +29,108 @@ public class UserRegisterTest extends BaseTestCase {
     private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
     private DataGenerator dataGenerator = new DataGenerator();
 @Test
-@Description("This test check user registration with existing email in DB")
-@DisplayName("Test negative registration user")
+@Description("This test check user registration with existing email")
+@DisplayName("Test negative registration user with existing email")
     public void testCreateUserExistingEmail(){
     String email = "vinkotov@example.com";
-    Map<String, String>  nonDefaultEmail = new HashMap<>();
-    nonDefaultEmail.put("email",  email);
-    Map<String, String> userData = dataGenerator.getRegistrationData(nonDefaultEmail);
+    Map<String, String>  existingEmail = new HashMap<>();
+    existingEmail.put("email",  email);
+    Map<String, String> userData = dataGenerator.getRegistrationData(existingEmail);
 
     Response responseCreateAuth = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/", userData);
 
     Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
     Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
 }
+
+    @Test
+    @Description("This test check user registration with invalid email")
+    @DisplayName("Test negative registration user with invalid email")
+    public void testCreateUserInvalidEmail() {
+        String email = "vinkotovexample.com";
+        Response responseForCheck = apiCoreRequests.makePostRequestWithInvalidEmailParam("https://playground.learnqa.ru/api/user/", email);
+
+        Assertions.assertResponseCodeEquals(responseForCheck, 400);
+        Assertions.assertResponseTextEquals(responseForCheck, "Invalid email format");
+    }
+    @Description("This test check user registration with long name")
+    @DisplayName("Test negative user registration  with long name")
+    @ParameterizedTest
+    @ValueSource(strings = {"username", "firstName", "lastName"})
+    public void testCreateUserLongName(String condition) {
+        RequestSpecification spec = RestAssured.given();
+        spec.baseUri("https://playground.learnqa.ru/api/user/");
+        if (condition.equals("username")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithLongNameParam("https://playground.learnqa.ru/api/user/", "username");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The value of 'username' field is too long");
+        } else if (condition.equals("firstName")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithLongNameParam("https://playground.learnqa.ru/api/user/", "firstName");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The value of 'firstName' field is too long");
+        } else if (condition.equals("lastName")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithLongNameParam("https://playground.learnqa.ru/api/user/", "lastName");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The value of 'lastName' field is too long");
+        } else {
+            throw new IllegalArgumentException("Condition value is unknown " + condition);
+        }
+    }
+    @Description("This test check user registration with short name")
+    @DisplayName("Test negative user registration  with short name")
+    @ParameterizedTest
+    @ValueSource(strings = {"username", "firstName", "lastName"})
+    public void testCreateUserShortName(String condition) {
+        RequestSpecification spec = RestAssured.given();
+        spec.baseUri("https://playground.learnqa.ru/api/user/");
+        if (condition.equals("username")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithShortNameParam("https://playground.learnqa.ru/api/user/", "username");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The value of 'username' field is too short");
+        } else if (condition.equals("firstName")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithShortNameParam("https://playground.learnqa.ru/api/user/", "firstName");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The value of 'firstName' field is too short");
+        } else if (condition.equals("lastName")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithShortNameParam("https://playground.learnqa.ru/api/user/", "lastName");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The value of 'lastName' field is too short");
+        } else {
+            throw new IllegalArgumentException("Condition value is unknown " + condition);
+        }
+    }
+    @Description("This test check user registration with missed parameter")
+    @DisplayName("Test negative user registration  with missed parameter")
+    @ParameterizedTest
+    @ValueSource(strings = {"email", "password", "username", "firstName", "lastName"})
+    public void testCreateUserMissedParam(String condition) {
+        RequestSpecification spec = RestAssured.given();
+        spec.baseUri("https://playground.learnqa.ru/api/user/");
+
+        if (condition.equals("email")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithMissedParam("https://playground.learnqa.ru/api/user/", "email");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The following required params are missed: email");
+        } else if (condition.equals("password")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithMissedParam("https://playground.learnqa.ru/api/user/", "password");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The following required params are missed: password");
+        } else if (condition.equals("username")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithMissedParam("https://playground.learnqa.ru/api/user/", "username");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The following required params are missed: username");
+        } else if (condition.equals("firstName")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithMissedParam("https://playground.learnqa.ru/api/user/", "firstName");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The following required params are missed: firstName");
+        } else if (condition.equals("lastName")) {
+            Response responseForCheck = apiCoreRequests.makePostRequestWithMissedParam("https://playground.learnqa.ru/api/user/", "lastName");
+            Assertions.assertResponseCodeEquals(responseForCheck, 400);
+            Assertions.assertResponseTextEquals(responseForCheck, "The following required params are missed: lastName");
+        } else {
+            throw new IllegalArgumentException("Condition value is unknown " + condition);
+        }
+    }
     @Test
     @Description("This test check new user registration")
     @DisplayName("Test positive registration user")
